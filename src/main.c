b4724a37664a4cb3c4d8bf68d684545c8e28c513
danenders <danenders@gmail.com>
2022-02-09 10:47:44 -0500

    WaitForVblank improved

--- src/main.c
+++ src/main.c
@@ -401,9 +401,7 @@ static void IntrDummy(void)
 static void WaitForVBlank(void)
 {
     gMain.intrCheck &= ~INTR_FLAG_VBLANK;
-
-    while (!(gMain.intrCheck & INTR_FLAG_VBLANK))
-        ;
+    asm("swi 0x5");
 }
 
 void SetTrainerHillVBlankCounter(u32 *counter)
