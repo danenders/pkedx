4cc3437518741ea75e336a966ef3abff7f299e48
danenders <danenders@gmail.com>
2022-02-10 17:55:30 -0500

    B Button during surfing now sprints

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -629,8 +629,10 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
 
     if (gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_SURFING)
     {
-        // same speed as running
-        PlayerWalkFast(direction);
+        if (heldKeys & B_BUTTON)
+            PlayerWalkFaster(direction);
+        else
+            PlayerWalkFast(direction);
         return;
     }
 
