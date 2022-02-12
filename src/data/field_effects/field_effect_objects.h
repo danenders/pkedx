9f3c00c56797ab148d49a5b97f7bcf962d1d2a2f
danenders <danenders@gmail.com>
2022-02-11 20:20:48 -0500

    May field move, underwater, and surf added Surf blob palette indexed properly Surf blob palette changed to NPC_1

--- src/data/field_effects/field_effect_objects.h
+++ src/data/field_effects/field_effect_objects.h
@@ -216,7 +216,7 @@ static const union AnimCmd *const sAnimTable_SurfBlob[] =
 
 const struct SpriteTemplate gFieldEffectObjectTemplate_SurfBlob = {
     .tileTag = TAG_NONE,
-    .paletteTag = OBJ_EVENT_PAL_TAG_MAY,
+    .paletteTag = OBJ_EVENT_PAL_TAG_NPC_1,
     .oam = &gObjectEventBaseOam_32x32,
     .anims = sAnimTable_SurfBlob,
     .images = sPicTable_SurfBlob,
