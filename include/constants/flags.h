f9685f635dcaed8934e2ce907658da6283d4464a
danenders <danenders@gmail.com>
2022-03-12 10:21:40 -0500

    Solved issues with Altering Cave

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -96,8 +96,8 @@
 #define FLAG_RESCUED_BIRCH                       0x52
 #define FLAG_LEGENDARIES_IN_SOOTOPOLIS           0x53
 
-#define FLAG_UNUSED_0x054                    0x54  // Unused Flag
-#define FLAG_UNUSED_0x055                    0x55  // Unused Flag
+#define FLAG_LEAF_OUTSIDE_AC                    0x54
+#define FLAG_LEAF_INSIDE_AC                    	0x55
 
 #define FLAG_HIDE_CONTEST_POKE_BALL          0x56  // Always set after new game, object it hides is added directly
 #define FLAG_MET_RIVAL_MOM                   0x57
